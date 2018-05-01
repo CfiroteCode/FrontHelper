d9571a834bcc9d4498cf590d59080529cb2764b2
Loison Michael <cfirote@gmail.com>
2018-05-01 13:06:09 +0200

    ajout de fonctionnalitée dans le canvas.

--- training canvas/canvasDessin.js
+++ training canvas/canvasDessin.js
@@ -4,10 +4,14 @@ var typeFonction = 0;
 var enregistrement = [];
 var btn1 = document.getElementById('1');
 var btn2 = document.getElementById('2');
+var moins = 0;
+var plus = 0;
 
 
 canvas.width = document.getElementById("canvasZone").clientWidth;
 canvas.height = document.getElementById("canvasZone").clientHeight;
+
+plus = canvas.width;
 
 function writeMessage(canvas, message) {
   context.clearRect(0, 0, canvas.width, canvas.height);
@@ -22,46 +26,81 @@ function getMousePos(canvas, evt) {
     y: evt.clientY - rect.top
   };
 }
-function dessine(evt){
+function dessine(evt) {
   var mousePos = getMousePos(canvas, evt);
-  if(typeFonction==0){
-  enregistrement.push([0,typeFonction,mousePos.y]);
-  }else if (typeFonction==1){
-  enregistrement.push([0,typeFonction,mousePos.x]);
+  if (typeFonction == 0) {
+    enregistrement.push([0, typeFonction, mousePos.y, plus, moins]);
+  } else if (typeFonction == 1) {
+    enregistrement.push([0, typeFonction, mousePos.x, plus, moins]);
   }
   compteur++;
-  
+
 }
 canvas.addEventListener('click', dessine);
 
-btn1.addEventListener('click', function(){
+btn1.addEventListener('click', function () {
   typeFonction = 0
-},false)
-btn2.addEventListener('click', function(){
+}, false)
+btn2.addEventListener('click', function () {
   typeFonction = 1
-},false)
+}, false)
 
 canvas.addEventListener('mousemove', function (evt) {
   var mousePos = getMousePos(canvas, evt);
-  var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y + ' , '+ enregistrement.length;
+  var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y + ' , ' + moins + ' , ' + plus;
   writeMessage(canvas, message);
   context.beginPath();
-    for (i=0; i<enregistrement.length;i++){
+  for (i = 0; i < enregistrement.length; i++) {
 
-      if(enregistrement[i][1]==0){
-      context.moveTo(0,enregistrement[i][2]);
-      context.lineTo(canvas.width, enregistrement[i][2]);
-      }else{
-      context.moveTo(enregistrement[i][2],0);
-      context.lineTo(enregistrement[i][2],canvas.height);
+    if (enregistrement[i][1] == 0) {
+      context.moveTo(enregistrement[i][4], enregistrement[i][2]);
+      context.lineTo(enregistrement[i][3], enregistrement[i][2]);
+    } else {
+      context.moveTo(enregistrement[i][2], enregistrement[i][4]);
+      context.lineTo(enregistrement[i][2], enregistrement[i][3]);
+    }
+  }
+  if (typeFonction == 0) {
+    if (enregistrement.length >= 1) {
+      moins = 0;
+      plus = canvas.width;
+      for (i = 0; i < enregistrement.length; i++) {
+        if (enregistrement[i][1] == 1) {
+          if (enregistrement[i][3] > mousePos.y && enregistrement[i][4] < mousePos.y) {
+            if (enregistrement[i][2] > moins && enregistrement[i][2] < mousePos.x) {
+
+              moins = enregistrement[i][2];
+            } else if (enregistrement[i][2] < plus && enregistrement[i][2] > mousePos.x) {
+
+              plus = enregistrement[i][2];
+            }
+
+          }
+        }
       }
     }
-    if(typeFonction == 0){
-  context.moveTo(0, mousePos.y);
-  context.lineTo(canvas.width, mousePos.y);
-    }else if (typeFonction == 1){
-  context.moveTo(mousePos.x,0);
-  context.lineTo(mousePos.x, canvas.height);
+    context.moveTo(moins, mousePos.y);
+    context.lineTo(plus, mousePos.y);
+  } else if (typeFonction == 1) {
+    if (enregistrement.length >= 1) {
+      moins = 0;
+      plus = canvas.width;
+      for (i = 0; i < enregistrement.length; i++) {
+        if (enregistrement[i][1] == 0) {
+          if (enregistrement[i][3] > mousePos.x && enregistrement[i][4] < mousePos.x) {
+            if (enregistrement[i][2] > moins && enregistrement[i][2] < mousePos.y) {
+
+              moins = enregistrement[i][2];
+
+            } else if (enregistrement[i][2] < plus && enregistrement[i][2] > mousePos.y) {
+              plus = enregistrement[i][2];
+            }
+          }
+        }
+      }
     }
+    context.moveTo(mousePos.x, moins);
+    context.lineTo(mousePos.x, plus);
+  }
   context.stroke();
 }, false);
