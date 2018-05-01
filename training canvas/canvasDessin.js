62b7a186e4fe7fb15933812814c575a1e8600b9e
Loison Michael <cfirote@gmail.com>
2018-05-02 00:08:52 +0200

    ajout de la fonction retour

--- training canvas/canvasDessin.js
+++ training canvas/canvasDessin.js
@@ -4,6 +4,7 @@ var typeFonction = 0;
 var enregistrement = [];
 var btn1 = document.getElementById('1');
 var btn2 = document.getElementById('2');
+var btn3 = document.getElementById('3');
 var moins = 0;
 var plus = 0;
 
@@ -13,20 +14,20 @@ canvas.height = document.getElementById("canvasZone").clientHeight;
 
 plus = canvas.width;
 
-function writeMessage(canvas, message) {
+function writeMessage(canvas, message) { //debug de la position de la souris, et des var plus et moins
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.font = '18pt Calibri';
   context.fillStyle = 'black';
   context.fillText(message, 10, 25);
 }
-function getMousePos(canvas, evt) {
+function getMousePos(canvas, evt) { // recupere la position de la souris sur le canvas
   var rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
     y: evt.clientY - rect.top
   };
 }
-function dessine(evt) {
+function dessine(evt) { //ajoute une ligne à la liste des ellement à afficher et l'enregistre dans l'array 2d
   var mousePos = getMousePos(canvas, evt);
   if (typeFonction == 0) {
     enregistrement.push([0, typeFonction, mousePos.y, plus, moins]);
@@ -36,21 +37,28 @@ function dessine(evt) {
   compteur++;
 
 }
-canvas.addEventListener('click', dessine);
+canvas.addEventListener('click', dessine); //detecte le clic sur le canvas
 
-btn1.addEventListener('click', function () {
-  typeFonction = 0
+btn1.addEventListener('click', function () {//detecte le clic sur le boutton 1
+  typeFonction = 0;
 }, false)
-btn2.addEventListener('click', function () {
-  typeFonction = 1
+btn2.addEventListener('click', function () {//detecte le clic sur le boutton 2
+  typeFonction = 1;
+}, false)
+btn3.addEventListener('click', function (){  //crée la fonction de UNDO
+  enregistrement.splice(enregistrement.length-1,1)
+  moins = 0;
+  plus = canvas.width;
 }, false)
 
-canvas.addEventListener('mousemove', function (evt) {
+
+
+canvas.addEventListener('mousemove', function (evt) {//fonction principale qui fonctionne tout le temp dés que la souris est sur le canvas
   var mousePos = getMousePos(canvas, evt);
-  var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y + ' , ' + moins + ' , ' + plus;
+  var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y + ' , ' + moins + ' , ' + plus + ' , ' + enregistrement.length;
   writeMessage(canvas, message);
   context.beginPath();
-  for (i = 0; i < enregistrement.length; i++) {
+  for (i = 0; i < enregistrement.length; i++) { //affiche les lignes enregistré si il y en a
 
     if (enregistrement[i][1] == 0) {
       context.moveTo(enregistrement[i][4], enregistrement[i][2]);
@@ -60,7 +68,7 @@ canvas.addEventListener('mousemove', function (evt) {
       context.lineTo(enregistrement[i][2], enregistrement[i][3]);
     }
   }
-  if (typeFonction == 0) {
+  if (typeFonction == 0) { // vérifie si on est en mode ajoute de ligne Horizontal
     if (enregistrement.length >= 1) {
       moins = 0;
       plus = canvas.width;
@@ -79,9 +87,10 @@ canvas.addEventListener('mousemove', function (evt) {
         }
       }
     }
+    context.strokeStyle = 'black';
     context.moveTo(moins, mousePos.y);
     context.lineTo(plus, mousePos.y);
-  } else if (typeFonction == 1) {
+  } else if (typeFonction == 1) { // vérifie si on est en mode ajoute de ligne vertical
     if (enregistrement.length >= 1) {
       moins = 0;
       plus = canvas.width;
@@ -99,6 +108,7 @@ canvas.addEventListener('mousemove', function (evt) {
         }
       }
     }
+    context.strokeStyle = 'black';
     context.moveTo(mousePos.x, moins);
     context.lineTo(mousePos.x, plus);
   }
